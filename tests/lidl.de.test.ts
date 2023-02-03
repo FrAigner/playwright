e3ace536a84fb9bc96b52822d939bd54fc538cc8
Frank4383 <Frank_Jakobs@hotmail.com>
2023-02-03 21:22:18 +0100

    Update lidl.de.test.ts

--- tests/lidl.de.test.ts
+++ tests/lidl.de.test.ts
@@ -1,1 +1,3 @@
-import { test, expect } from '@playwright/test'; test('lidl.de', async({ page })) => { await page.goto('https://www.lidl.de') };
+import { test, expect } from '@playwright/test'; 
+test('lidl.de', async({ page }) => { 
+  await page.goto('https://www.lidl.de') });
